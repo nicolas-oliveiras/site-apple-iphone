03ccbd1d9dd34486aef6db01e0f106bc58cd6a44
nicolas-oliveiras <niick.7483@gmail.com>
2023-03-19 19:26:04 -0300

    site-apple-iphone

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function tradeColor (color) {
+    let circle = document.querySelector(".circle")
+    circle.style.background = color
+}
+
+function tradeImage (image){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = image
+}
